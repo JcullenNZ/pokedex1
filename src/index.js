24f7cee48693c082fc8595fd3cf5a06afb26bfd4
James Cullen <jcullen.dk@gmail.com>
2023-04-07 13:11:23 +0200

    - Created route components - Navigation is now functional -->index creates the hashrouter -->Header is the parent, with each route the children of it, these are rendered under <Outlet /> in the Header component - Will need a tidy up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createHashRouter } from "react-router-dom"
 
 import './index.css';
 
-import Root from "./Components/Root";
-import RenderPokemons from "./Components/LoadPokemon";
+import App from "./App";
+import Pokedex from "./Routes/Pokedex";
+import Home from "./Routes/Home";
+import About from "./Routes/About";
+import Search from "./Routes/Search";
+import Content from "./Components/Content";
+import Header from "./Components/Header";
 
+const router = createHashRouter([
+    {
+        path: "/",
+        element: <Header />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/pokedex",
+                element: <Pokedex />
+            },
+            {
+                path: "/search",
+                element: <Search />
+            }
+        ],
+    },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Root />
-      <RenderPokemons />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
+//<RenderPokemons />
